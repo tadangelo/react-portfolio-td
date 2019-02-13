d0ca746a7acc81df2ca95b06407e1f48f25ee828
Tania D'Angelo <tania.a.dangelo@outlook.com>
2019-02-13 08:13:35 -0600

    Added functional component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Tania D'Angelo</h1>
-        <h2>Software Programmer</h2>
-        <div>
-          {moment().format('MMMM Do YYYY')}
-        </div>
+        <h3>Software Programmer</h3>
+        <div>{moment().format('MMMM Do YYYY')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
