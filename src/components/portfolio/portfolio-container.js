b7eb140a41bbcfb53f8a303a881111a825670f69
Tania D'Angelo <tania.a.dangelo@outlook.com>
2019-02-14 23:22:57 -0600

    added props tested responsiveness

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,12 +5,26 @@ import PortfolioItem from './portfolio-item';
 export default class PortfolioContainer extends Component {
     //State
     //Lifecycle hooks
+    constructor() {
+        super();
+        console.log("Portfolio container has rendered")
+    }
+
+    portfolioItems() {
+        const data = ["CGI", "Bottega", "DXC Technology", "Freelance Development"];
+        
+        return data.map(item => {
+            return <PortfolioItem title={item} url={"google.com"} />;
+            // return <h2>{item}</h2>;
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>Portfolio items go here....</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         )
     }
