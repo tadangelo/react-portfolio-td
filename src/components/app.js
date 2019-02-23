3a64a45886880aaaef7bac8e6ccbba48a49d053c
Tania D'Angelo <tania.a.dangelo@outlook.com>
2019-02-22 23:51:53 -0600

    Added routes to home and about pages

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
+import Home from "./pages/home";
+import About from "./pages/about";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <Router>
+        <div>
+          <NavigationContainer />
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/about" component={ About } />
+          </Switch>
+        </div>
+        </Router>
+
+
         <h1>Tania D'Angelo</h1>
         <h3>Software Programmer</h3>
         <div>{moment().format('MMMM Do YYYY')}</div>
